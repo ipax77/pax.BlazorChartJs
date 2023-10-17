5af1d6a17a53ba3c65a7e28e11eb4e16fc025e57
ipax77 <ph.ilipp@web.de>
2023-10-17 07:55:07 +0200

    dotnet 8 rc.2

--- src/pax.BlazorChartJs.wasmtest/Program.cs
+++ src/pax.BlazorChartJs.wasmtest/Program.cs
@@ -1,14 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using pax.BlazorChartJs.wasmtest.Components;
 using pax.BlazorChartJs;
-using pax.BlazorChartJs.wasmtest;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddChartJs();
 
-await builder.Build().RunAsync().ConfigureAwait(false);
+await builder.Build().RunAsync();
