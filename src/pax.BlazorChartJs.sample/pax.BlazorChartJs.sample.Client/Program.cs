5df5636eafdccf05a822e3d98179cc5bca1e7af7
ipax77 <ph.ilipp@web.de>
2025-09-24 11:56:53 +0200

    add Blazor Server sample project

--- src/pax.BlazorChartJs.sample/pax.BlazorChartJs.sample.Client/Program.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using pax.BlazorChartJs;
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-builder.Services.AddScoped(sp => new HttpClient());
-builder.Services.AddChartJs();
-
-await builder.Build().RunAsync();
