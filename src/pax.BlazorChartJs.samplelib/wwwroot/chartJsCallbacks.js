8859d8f8a93a5c8901346f9ef28484b01c50f199
ipax77 <ph.ilipp@web.de>
2026-05-19 11:44:38 +0200

    allow ChartJsFunction for IndexableOption

--- src/pax.BlazorChartJs.samplelib/wwwroot/chartJsCallbacks.js
+++ src/pax.BlazorChartJs.samplelib/wwwroot/chartJsCallbacks.js
@@ -1,3 +1,28 @@
+const fillPatternCache = new WeakMap();
+
+function createFillPattern(chart) {
+    let pattern = fillPatternCache.get(chart);
+    if (pattern) {
+        return pattern;
+    }
+
+    const patternCanvas = document.createElement('canvas');
+    patternCanvas.width = 8;
+    patternCanvas.height = 8;
+
+    const patternContext = patternCanvas.getContext('2d');
+    patternContext.fillStyle = '#2563eb';
+    patternContext.fillRect(0, 0, 8, 8);
+    patternContext.fillStyle = '#facc15';
+    patternContext.fillRect(0, 0, 4, 4);
+    patternContext.fillRect(4, 4, 4, 4);
+
+    pattern = chart.ctx.createPattern(patternCanvas, 'repeat');
+    fillPatternCache.set(chart, pattern);
+
+    return pattern;
+}
+
 const callbacks = Object.assign(Object.create(null), {
     formatPercent(value, context) {
         return `${Math.round(value * 100)}%`;
@@ -10,6 +35,9 @@ const callbacks = Object.assign(Object.create(null), {
     },
     labelWithIndex(value, context) {
         return `${context.dataIndex}: ${value}`;
+    },
+    createRepeatFillPattern(context) {
+        return createFillPattern(context.chart);
     }
 });
 
