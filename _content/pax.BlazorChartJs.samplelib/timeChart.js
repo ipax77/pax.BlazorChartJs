ca15d8080087e36d6a8ff0899cd402f76344d9cc
ipax77 <ipax77@users.noreply.github.com>
2026-05-27 05:02:15 +0000

    deploy: 6080e46640c14bf60e4653efa34695950a576f11

--- _content/pax.BlazorChartJs.samplelib/timeChart.js
+++ _content/pax.BlazorChartJs.samplelib/timeChart.js
@@ -1,6 +1,14 @@
-﻿import './chartjs-adapter-date-fns.bundle.min.js'
+let registrationPromise;
 
-export function registerPlugin() {
-
+export function registerPlugin(chartJsLocation) {
+    registrationPromise ??= registerPluginCore(chartJsLocation);
+    return registrationPromise;
 }
 
+async function registerPluginCore(chartJsLocation) {
+    if (typeof globalThis.Chart === 'undefined' && chartJsLocation) {
+        await import(chartJsLocation);
+    }
+
+    await import('./chartjs-adapter-date-fns.bundle.min.js');
+}
